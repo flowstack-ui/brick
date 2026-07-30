267f6ce0fcdeec64ee6529691b2b195dcf4355cf
Will Donin <guilherme@donin.com.br>
2026-07-30 18:35:08 -0400

    test: advance Atom dependency invariant

--- test/package/package.test.mjs
+++ test/package/package.test.mjs
@@ -10,7 +10,7 @@ test("package metadata defines the public Brick boundary", async () => {
   );
 
   assert.equal(packageJson.name, "@flowstack-ui/brick");
-  assert.equal(packageJson.dependencies["@flowstack-ui/atom"], "0.20.1");
+  assert.equal(packageJson.dependencies["@flowstack-ui/atom"], "0.20.2");
   assert.equal(
     packageJson.repository.url,
     "git+https://github.com/flowstack-ui/brick.git",
